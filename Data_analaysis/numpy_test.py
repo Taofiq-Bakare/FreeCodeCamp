5df39c22306c29e6f9dd4a12d8effc4cd7581590
SirTfk <54087879+SirTfk@users.noreply.github.com>
2021-01-30 13:12:07 +0800

    All I needed to do was convert using .tolist()

--- Data_analaysis/numpy_test.py
+++ Data_analaysis/numpy_test.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numpy.core.records import array
+
+# from numpy.core.records import array
 # from numpy.core.fromnumeric import mean
 
 
@@ -24,9 +25,9 @@ def calc_mean(matrix):
     """
     A function to calculate the mean for the axis
     """
-    all_mean = [str(np.mean(matrix, axis=0)),
-                str(np.mean(matrix, axis=1)),
-                np.mean(matrix)
+    all_mean = [(np.mean(matrix, axis=0).tolist()),
+                (np.mean(matrix, axis=1).tolist()),
+                np.mean(matrix).tolist()
                 ]
     return all_mean
 
@@ -35,9 +36,9 @@ def calc_var(matrix):
     """
     A function to calculate the variance for the axis
     """
-    all_var = [str(np.var(matrix, axis=0)),
-               str(np.var(matrix, axis=1)),
-               np.var(matrix)
+    all_var = [(np.var(matrix, axis=0).tolist()),
+               (np.var(matrix, axis=1).tolist()),
+               np.var(matrix).tolist()
                ]
     return all_var
 
@@ -46,9 +47,9 @@ def calc_std(matrix):
     """
     A function to calculate the std for the axis
     """
-    all_std = [str(np.std(matrix, axis=0)),
-               str(np.std(matrix, axis=1)),
-               np.std(matrix)
+    all_std = [(np.std(matrix, axis=0).tolist()),
+               (np.std(matrix, axis=1).tolist()),
+               np.std(matrix).tolist()
                ]
     return all_std
 
@@ -57,9 +58,9 @@ def calc_max(matrix):
     """
     A function to calculate the max for the axis
     """
-    all_max = [str(np.max(matrix, axis=0)),
-               str(np.max(matrix, axis=1)),
-               np.max(matrix)
+    all_max = [(np.max(matrix, axis=0).tolist()),
+               (np.max(matrix, axis=1).tolist()),
+               np.max(matrix).tolist()
                ]
     return all_max
 
@@ -68,9 +69,9 @@ def calc_min(matrix):
     """
     A function to calculate the min for the axis
     """
-    all_min = [str(np.min(matrix, axis=0)),
-               str(np.min(matrix, axis=1)),
-               np.min(matrix)
+    all_min = [(np.min(matrix, axis=0).tolist()),
+               (np.min(matrix, axis=1).tolist()),
+               np.min(matrix).tolist()
                ]
     return all_min
 
@@ -79,35 +80,43 @@ def calc_sum(matrix):
     """
     A function to calculate the sum for the axis
     """
-    all_sum = [str(np.sum(matrix, axis=0)),
-               str(np.sum(matrix, axis=1)),
-               np.sum(matrix)
+    all_sum = [(np.sum(matrix, axis=0).tolist()),
+               (np.sum(matrix, axis=1).tolist()),
+               np.sum(matrix).tolist()
                ]
     return all_sum
 
 
 def calculate(list):
     """
-    A function to collate the results into a dictionary
+      A function to collate the results into a dictionary
     """
-
+    # if len(list) != 9:
+    #   return "List must contain nine numbers."
     try:
         matrcs = to_mat(list)
-        solutions = {'mean': calc_mean(matrcs),
-                     'variance': calc_var(matrcs),
-                     'standard deviation': calc_std(matrcs),
-                     'max': calc_max(matrcs),
-                     'min': calc_min(matrcs),
-                     'sum': calc_sum(matrcs),
-                     }
-        return solutions
+        calculations = {'mean': calc_mean(matrcs),
+                        'variance': calc_var(matrcs),
+                        'standard deviation': calc_std(matrcs),
+                        'max': calc_max(matrcs),
+                        'min': calc_min(matrcs),
+                        'sum': calc_sum(matrcs),
+                        }
+        return calculations
     except ValueError:
-        print("The list is less 9")
+        return "List must contain nine numbers."
 
 
-number = [9, 1, 5, 3, 3, 3, 2, 9, 0]
+# number = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+# solution_1 = calculate(number)
+number = [2, 6, 2, 8, 4, 0, 1, 9]
 
 solution_1 = calculate(number)
 
-for key, value in solution_1.items():
-    print(key, str(value))
+# for key, value in solution_1.items():
+#     print(f"{key}, {value}")
+
+# for key in solution_1:
+#     print(f"{key};{solution_1[key]}")
+print(solution_1)
